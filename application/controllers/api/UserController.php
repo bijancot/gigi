b90eb758e53b2368bc4c9e20c73e305690cbff65
uzenn <andrewnagatha@gmail.com>
2021-10-13 23:39:52 +0700

    validation remove

--- application/controllers/api/UserController.php
+++ application/controllers/api/UserController.php
@@ -15,34 +15,18 @@
             ], 200);
         }
         public function register_post() {
-            // $this->form_validation->set_rules('email','Email','required|is_unique[user.email]', array(
-            //     'is_unique' => '%s already exists.'
-            // ));
-            $this->form_validation->set_rules('email','Email','required');
-            $this->form_validation->set_rules('name','Name','required');
-            $this->form_validation->set_rules('gender','Gender','required');
-            $this->form_validation->set_rules('birth_date','Birth Date','required');
-            $this->form_validation->set_rules('phone_number','Phone Number','required');
-            $this->form_validation->set_rules('school_name','School Name','required');
-            $this->form_validation->set_rules('school_class','School Class','required');
-            $this->form_validation->set_rules('password', 'Password', 'required');
-
-            if($this->form_validation->run() == TRUE) {
-                $arr = array(
-                    'email' => $this->post('email'),
-                    'name' => $this->post('name'),
-                    'gender' => $this->post('gender'),
-                    'birth_date' => date_format(date_create($this->post('birth_date')), "Y-m-d"),
-                    'phone_number' => $this->post('phone_number'),
-                    'school_name' => $this->post('school_name'),
-                    'school_class' => $this->post('school_class'),
-                    'password' => md5($this->post('password'))
-                );
-                $this->User->insertUser($arr);
-                $this->response(['status' => true, 'message' => 'Register successfully'], 200);
-            } else {
-                $this->response(['status' => false, 'message' => 'Register failed, '.validation_errors()], 404);
-            }                
+            $arr = array(
+                'email' => $this->post('email'),
+                'name' => $this->post('name'),
+                'gender' => $this->post('gender'),
+                'birth_date' => date_format(date_create($this->post('birth_date')), "Y-m-d"),
+                'phone_number' => $this->post('phone_number'),
+                'school_name' => $this->post('school_name'),
+                'school_class' => $this->post('school_class'),
+                'password' => md5($this->post('password'))
+            );
+            $this->User->insertUser($arr);
+            $this->response(['status' => true, 'message' => 'Register successfully'], 200);           
         }
         public function login_post() {
             $email = $this->post('email');
